3b5371b1dfa17b01133273cd11e11760283b585d
basketdelgado <f.basket1@gmail.com>
2016-12-07 10:39:47 +0100

    clases creadas

---
+++ tema 5/worksheet5/stickyNotes.js
@@ -0,0 +1,41 @@
+class Vista{
+    constructor(controlador){
+        this.controlador = controlador;
+    }
+}
+
+class Controlador{
+    cambiarPosicion(){
+
+    }
+
+    modificarContenido(){
+
+    }
+
+    cambiarTamaño(){
+
+    }
+}
+
+class Modelo{
+    cambiarPosicion(){
+
+    }
+
+    modificarPosicion(){
+
+    }
+
+    TiempoTranscurrido(){
+
+    }
+}
+
+class Nota{
+    constructor(){
+        this.titulo;
+        this.texto;
+        this.fechaCreacion;
+    }
+}
