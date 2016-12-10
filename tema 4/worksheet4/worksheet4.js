e8c3a1ea2f4cbd12c28ae3b1f0d76573b4fb8a52
basketdelgado <f.basket1@gmail.com>
2016-12-10 16:12:09 +0100

    terminado

--- tema 4/worksheet4/worksheet4.js
+++ tema 4/worksheet4/worksheet4.js
@@ -1,39 +1,61 @@
-function obtenerCookie(nombre) {
-    var usuario = nombre + "=";
-    var datos = document.cookie.split(';');
-    for(var i = 1; i <datos.length; i++) {
-        var dato = datos[i];
-        while (dato.charAt(0)==' ') {
-            dato = dato.substring(1);
-        }
-        if (dato.indexOf(usuario) == 0) {
-            var valor = nombre + "=";
-            return dato.substring(valor.length,dato.length);
-        }
-    }
-    return "";
-}
-function crearCookie(usuario, valor, expiracion) {
+function crearCookie(usuario, nombre, expiracion) {
     var d = new Date();
     d.setTime(d.getTime() + (expiracion*24*60*60*1000));
     var expires = "expires="+ d.toUTCString();
-    document.cookie = usuario + "=" + valor + ";" + expires + ";path=/";
+    document.cookie = usuario + "=" + nombre + ";" + expires + ";path=/";
 }
+
+function obtenerCookie(nombre) {
+            var name = nombre + "=";
+            var datos = document.cookie.split(';');
+            for (var i = 0; i < datos.length; i++) {
+                    while (dato.charAt(0) == ' ') {
+                        dato = dato.substring(1);
+                    }
+                    if (dato.length != 0) {
+                        return dato.substring(name.length, dato.length);
+                    }
+            }
+    return "";
+}
+
 function comprobarcookie() {
-    var usuario=obtenerCookie("usuario");
-    if (usuario!="") {
+    var fondo = obtenerCookie("fondo");
+    var parrafo = obtenerCookie("parrafo");
+    var tamaño = obtenerCookie("tamaño");
+    var usuario = obtenerCookie("usuario");
+    if (usuario != "") {
         document.getElementById("saludo").innerHTML = "hola " + usuario;
+        var cuerpo = document.getElementById("cuerpo");
+        cuerpo.style.backgroundColor(fondo);
+
     } else {
         usuario = prompt("introduce nombre:", "");
-        if (usuario != "") {
+        if (usuario != ""  && usuario != null) {
             var fecha = new Date();
-            fecha.setTime(fecha.getTime() + (5*60*1000));
+            fecha.setTime(fecha.getTime() + (5*24*60*60*1000));
             crearCookie("usuario", usuario, fecha);
+            preferencias();
+
         }
     }
 }
 
-function personalizacion(){
+function preferencias(){
+    var formulario = document.forms["formulario"];
+    var fecha = new Date();
+    fecha.setTime(fecha.getTime() + (5*24*60*60*1000));
+    if(formulario.fondo.value != ""){
+        crearCookie("fondo",formulario.fondo.value,fecha);
+    }
+
+    if(formulario.parrafo.value != ""){
+        crearCookie("parrafo",formulario.parrafo.value,fecha);
+    }
+
+    if(formulario.tamaño.value != "") {
+        crearCookie("tamaño", formulario.tamaño.value, fecha);
+    }
 }
 
 window.onload = function(){
