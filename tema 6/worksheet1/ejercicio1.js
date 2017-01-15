1c0622d9c2597febdd726624b7f68e62ded200ba
basketdelgado <f.basket1@gmail.com>
2017-01-15 23:28:03 +0100

    finalizado

--- tema 6/worksheet1/ejercicio1.js
+++
@@ -1,4 +0,0 @@
-/**
- * Crea una página web con un botón, que haciendo uso de AJAX al pulsarlo se llame a
- * otra página del servidor y se muestre su contenido.
- */
--- tema 6/worksheet2/ejercicio1.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by javi on 07/12/2016.
- */
