0d20a83c66d0730b90704b8ebe5f48a774dfa26d
basketdelgado <f.basket1@gmail.com>
2017-01-19 21:07:04 +0100

    carga datos API

--- Evaluacion 2/Tema 7/OMBD/main.js
+++ Evaluacion 2/Tema 7/OMBD/main.js
@@ -1,7 +1,18 @@
 
 class Vista{
-    constructor(){
+    constructor(controlador){
+        this.controlador = controlador;
 
+    }
+    addEventoBuscar(){
+        var that = this;
+        $("#buscar").click(function () {
+            that.buscarPeliculas();
+        });
+    }
+    buscarPeliculas(){
+        this.titulo = $("#titulo").val();
+        this.controlador.obtenerPeliculas(this.titulo,"http://www.omdbapi.com/?");
     }
 }
 
@@ -9,35 +20,40 @@ class Controlador{
     constructor(){
         this.modelo = new Modelo(this);
         this.vista = new Vista(this);
+        this.pagina = 1;
+        this.vista.addEventoBuscar();
+        }
+        obtenerPeliculas(titulo,api){
+            var that = this;
+            $.getJSON(api,{
+                s:titulo,
+                //type:tipo,
+                page:this.pagina
+            }, function (data) {
+                that.modelo.cargarDatosPeliculas(data);
+            });
+            this.pagina ++;
+        }
+    imprimirPelicula(poster,titulo,año){
+        var caja = document.createElement("div");
+        var
     }
 }
 
 class Modelo{
     constructor(controlador){
         this.controlador = controlador;
-        this.conexion = new Conexion();
+    }
+    cargarDatosPeliculas(data){
+        var that = this;
+        $.each(data["Search"], function (index,item) {
+            console.log(item);
+            that.controlador.imprimirPelicula(item.Poster,item.Title,item.Year);
+        })
     }
 }
 
-class Conexion{
-    constructor(api){
-        this.api = api;
-    }
-
-}
-
-class Pelicula{
-    constructor(){
-
-    }
-}
-
-class Comic{
-    constructor(){
-
-    }
-}
 
 $(document).ready(function(){
-    var controlador = Controlador();
+    var controlador = new Controlador();
 });
