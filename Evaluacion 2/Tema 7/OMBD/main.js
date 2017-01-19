6df43027c4237fd4886dcd589d182f50946c6ef0
basketdelgado <f.basket1@gmail.com>
2017-01-20 00:13:12 +0100

    solucionar scroll

--- Evaluacion 2/Tema 7/OMBD/main.js
+++ Evaluacion 2/Tema 7/OMBD/main.js
@@ -4,26 +4,43 @@ class Vista{
         this.controlador = controlador;
         this.cuerpo = $("section");
     }
+
     EventoBuscar(){
         var that = this;
         $("#buscar").click(function () {
             that.buscarPeliculas();
         });
     }
+
     buscarPeliculas(){
         this.pagina = 1;
         this.titulo = $("#titulo").val();
         this.controlador.obtenerPeliculas(this.titulo,this.pagina,"http://www.omdbapi.com/?");
+        this.pagina ++;
     }
+
     mostrarPelicula(pelicula){
         $(this.cuerpo).append(pelicula);
         this.mostrarMasDatos();
     }
+
     mostrarMasDatos(){
-    $("section").infinitescroll(function () {
-        this.controlador.obtenerPeliculas(this.titulo,this.pagina,"http://www.omdbapi.com/?");
-    });
-        this.pagina ++;
+        var that = this;
+        var ventana = $(window);
+        ventana.scroll(function () {
+            if($(window).scrollTop() + $(window).height() == that.getDocHeight()) {
+                that.controlador.obtenerPeliculas(this.titulo, this.pagina, "http://www.omdbapi.com/?");
+                that.pagina++;
+            }
+        });
+    }
+    getDocHeight() {
+        var D = document;
+        return Math.max(
+            D.body.scrollHeight, D.documentElement.scrollHeight,
+            D.body.offsetHeight, D.documentElement.offsetHeight,
+            D.body.clientHeight, D.documentElement.clientHeight
+        );
     }
 }
 
@@ -32,17 +49,19 @@ class Controlador{
         this.modelo = new Modelo(this);
         this.vista = new Vista(this);
         this.vista.EventoBuscar();
-        }
-        obtenerPeliculas(titulo,pagina,api){
-            var that = this;
-            $.getJSON(api,{
-                s:titulo,
-                //type:tipo,
-                page:pagina
-            }, function (data) {
-                that.modelo.cargarDatosPeliculas(data);
+    }
+
+    obtenerPeliculas(titulo,pagina,api){
+        var that = this;
+        $.getJSON(api,
+            {s:titulo, page:pagina},
+            function (data) {
+            that.modelo.cargarDatosPeliculas(data);
+        }, function () {
+                alert("no hay mas peliculas");
             });
-        }
+    }
+
     mostrarPelicula(pelicula){
         this.vista.mostrarPelicula(pelicula);
     }
@@ -52,12 +71,14 @@ class Modelo{
     constructor(controlador){
         this.controlador = controlador;
     }
+
     cargarDatosPeliculas(data){
         var that = this;
         $.each(data["Search"], function (index,item) {
             that.crearPelicula(item.Poster,item.Title,item.Year);
         });
     }
+
     crearPelicula(poster, title, year){
         var pelicula = document.createElement("article");
         var portada = document.createElement("img");
